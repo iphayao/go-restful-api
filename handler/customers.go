e978cd81b5e529e7b4670cb1b314e0f793c15dbf
iphayao <phayaob@gmail.com>
2019-05-22 22:29:02 +0700

    added all customer enpoints and handlers

--- handler/customers.go
+++ handler/customers.go
@@ -1,10 +1,49 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+func responseJSON(w http.ResponseWriter, status int, payload string) {
+	response, err := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
+
 func CustomerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, customer!")
+	responseJSON(w, 200, "hello, customer!")
+}
+
+func CustomerGetAllHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
+}
+
+func CustomerGetByIdHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
+}
+
+func CustomerGetByNameHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
+}
+
+func CustomerPostHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
+}
+
+func CustomerPutByIdHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
+}
+
+func CustomerDeleteByIdHandler(w http.ResponseWriter, r *http.Request) {
+	responseJSON(w, 200, "not implement yet!")
 }
--- main.go
+++ main.go
@@ -11,8 +11,15 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handler.HomeHandler)
-	r.HandleFunc("/customers", handler.CustomerHandler)
+
+	s := r.PathPrefix("/api").Subrouter()
+
+	s.HandleFunc("/customers", handler.CustomerGetAllHandler).Methods("GET")
+	s.HandleFunc("/customers/{id}", handler.CustomerGetByIdHandler).Methods("GET")
+	s.HandleFunc("/customers", handler.CustomerGetByNameHandler).Queries("name", "{name}").Methods("GET")
+	s.HandleFunc("/customers", handler.CustomerPostHandler).Methods("POST")
+	s.HandleFunc("/customers/{id}", handler.CustomerPutByIdHandler).Methods("PUT")
+	s.HandleFunc("/customers/{id}", handler.CustomerDeleteByIdHandler).Methods("DELETE")
 
 	fmt.Println("Server running ...")
 	log.Fatal(http.ListenAndServe(":8080", r))
